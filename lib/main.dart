a2f90979979a7764c873703fabcc6e538e7075ec
FlutLab.io <>
2022-05-25 04:48:44 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -103,7 +103,7 @@ class MyHomePage extends StatelessWidget {
                         child: const SizedBox(
                           width: 300,
                           height: 50,
-                          child: Text('Kaset Album Use Your Ilusion 2'),
+                          child: Text('Kaset Album Use Your Ilusion 2', style: TextStyle(fontWeight: FontWeight.bold)),
                         ),
                       ),
                     ),
